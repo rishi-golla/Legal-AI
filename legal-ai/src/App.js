0bc9967ec2bb6772db6cb6a1935000e69e4f2ff8
Arnab <official.arnabdev@gmail.com>
2024-09-20 19:30:07 -0500

    navbar

--- legal-ai/src/App.js
+++ legal-ai/src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-primary h-screen w-screen">
       <header className="App-header text-black">
           <Navbar/>
       </header>
--- legal-ai/tailwind.config.js
+++ legal-ai/tailwind.config.js
@@ -4,7 +4,31 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "960px",
+        xl: "1200px",
+    },
+    fontFamily: {
+      primary: "var(--font-jetbrainsMono)" ,
+      poppins: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      colors:{
+        primary:"#010319",
+        secondary:"#cbacf9",
+        light:"#492c74",
+        accent:{
+          DEFAULT: "#00ff99",
+          hover: "00e187"
+        }
+      },
+    },
   },
   plugins: [],
 }
