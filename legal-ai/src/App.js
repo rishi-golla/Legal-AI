6dd5c5b79bcc86f200f3236e82ecd672dd39f6c2
Arnab <official.arnabdev@gmail.com>
2024-09-20 22:15:08 -0500

    a

--- legal-ai/src/App.js
+++ legal-ai/src/App.js
@@ -4,8 +4,8 @@ import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div className="App bg-primary h-screen w-screen">
-      <header className="App-header text-black">
+    <div className="App bg-primary h-screen w-screen p-6">
+      <header className="App-header text-black w-full ">
           <Navbar/>
       </header>
     </div>
--- legal-ai/src/components/navbar.js
+++ legal-ai/src/components/navbar.js
@@ -1,8 +1,46 @@
 import React from 'react'
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "Legal Assistant",
+    path: "/legalassistant",
+  },
+  {
+    name: "Documentation Helper",
+    path: "documentationhelper",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
 
 const Navbar = () => {
   return (
-    <div className='text-white font-poppins '>Navbar</div>
+    <nav className='w-full flex-row flex justify-between'>
+        <div>
+            <img className='' width={80} height={80} src='logo.png'></img>
+        </div>
+        <div className="flex gap-8 flex-row  justify-center items-center">
+      
+            {links.map((link, index) => {
+                return (
+                <a
+                    href={link.path}
+                    key={index}
+                    className="text-[#cbacf9] border-b-2 border-[#cbacf9]
+                    capitalize font-medium hover:text-[#ffffff] transition-all duration 100"
+                >
+                    {link.name}
+                </a>
+                );
+            })}
+        </div>    
+    </nav>
+    
   )
 }
 
