f6b2fa0d539602c5ef58742a88d2ca9c429a5ecd
Arnab <official.arnabdev@gmail.com>
2024-09-21 13:54:39 -0500

    minor-changes

--- legal-ai/src/components/Header.js
+++ legal-ai/src/components/Header.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Header = ({ setActiveSection }) => {
+  const [hidden, setHidden] = useState(false); // Initially set to false to show the navbar
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const handleScroll = () => {
+    const scrollY = window.scrollY;
+
+    if (scrollY > lastScrollY) {
+      setHidden(true); // Scrolling down
+    } else {
+      setHidden(false); // Scrolling up
+    }
+    setLastScrollY(scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
-    <header className="bg-black bg-opacity-50 shadow-md fixed w-full top-0 left-0 z-10 flex justify-between items-center px-6 py-4">
+    <header className={`bg-black bg-opacity-50 shadow-md fixed w-full top-0 left-0 z-10 flex justify-between items-center px-6 py-4 transition-transform duration-300 ${hidden ? '-translate-y-full' : 'translate-y-0'}`}>
       <div className="flex items-center space-x-3">
-        {/* Add logo next to title */}
         <img
           src="https://cdn.discordapp.com/attachments/1286831898147946639/1286847170938667048/logo.png?ex=66ef6552&is=66ee13d2&hm=5a6f86b8d67930ea0c671e0edab662f06f3f61868b6431567e80f5d128c4f99e&"
           alt="logo"
@@ -14,7 +35,6 @@ const Header = ({ setActiveSection }) => {
           LegalAI
         </h1>
       </div>
-      {/* Centering the nav items */}
       <nav className="flex space-x-6 mx-auto">
         <button
           className="text-purple-300 hover:text-purple-400"
@@ -30,7 +50,7 @@ const Header = ({ setActiveSection }) => {
         </button>
       </nav>
       <div className="flex items-center space-x-2 relative group" onClick={() => setActiveSection("chatbot")}>
-      <span className="text-purple-300 hover:text-purple-400 cursor-pointer">Start Your Chat Here -></span>
+        <span className="md:flex hidden text-purple-300 hover:text-purple-400 cursor-pointer">Start Your Chat Here -></span>
         <div className="w-10 h-10 bg-indigo-500 rounded-full flex items-center justify-center cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-110">
           <img
             src="https://cdn.discordapp.com/attachments/1286831898147946639/1286847170938667048/logo.png?ex=66ef6552&is=66ee13d2&hm=5a6f86b8d67930ea0c671e0edab662f06f3f61868b6431567e80f5d128c4f99e&"
--- legal-ai/src/components/Home.js
+++ legal-ai/src/components/Home.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import About from './About';
+import { useInView } from 'react-intersection-observer';
 
 const Home = ({ setActiveSection }) => {
+  const { ref: goalRef, inView: goalInView } = useInView({ threshold: 0.5 });
+  const { ref: hackathonRef, inView: hackathonInView } = useInView({ threshold: 0.5 });
+
   return (
-    <section className="pt-20 min-h-screen bg-primary text-textPrimary flex flex-col justify-center items-center text-center  space-y-8">
-      <div className='flex flex-row'>
-        <div className='relative w-1/3 flex items-center justify-center pt-10'>
+    <section className="pt-20 min-h-screen bg-primary text-textPrimary flex flex-col justify-center items-center text-center space-y-8">
+      <div className='flex flex-row h-auto lg:min-h-screen'>
+        <motion.div
+          initial={{ x: '-100%', opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1}}
+          className='left relative w-1/3 lg:flex hidden items-center justify-center pt-10'
+        >
           <img
             src='lawyer.png'
             alt='Lawyer'
@@ -18,45 +28,86 @@ const Home = ({ setActiveSection }) => {
           </button>
           <button className='duration-150 bg-[#00000080]  hover:text-black absolute top-[80px] right-[180px] rounded-full p-2 text-purple-300'>
           </button>
+        </motion.div>
+        <div className='right gap-5 flex flex-col lg:w-2/3 w-full h-full justify-between'>
+          <motion.div
+            initial={{ x: '100%', opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.8 }}
+            className=' w-full flex flex-col gap-5 justify-center'
+          >
+            <div className="animate-fade-in w-full max-w-4xl p-6 bg-primary border border-accent rounded-lg transform transition duration-500 hover:scale-105 shadow-lg">
+              <h2 className="text-purple-300 text-5xl md:text-6xl font-bold text-accent mb-4 leading-tight">
+                Innovating <br /> Legal Assistance with AI
+              </h2>
+              <p className="text-purple-300 text-lg">
+                LegalAI provides cutting-edge AI solutions to answer your legal questions quickly and accurately.
+              </p>
+            </div>
+
+          </motion.div>
+          <motion.div
+            initial={{ x: '100%', opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.8, delay:0.5 }}
+            className=' w-full flex flex-col gap-5 justify-center'
+          >
+            <div className="animate-fade-in w-full max-w-4xl p-6 border border-accent rounded-lg transform transition duration-500 hover:scale-105 shadow-lg" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+              <p className="text-purple-300 text-lg">
+                Our goal is to make legal knowledge accessible, reliable, and efficient for everyone, by utilizing the latest advancements in AI technology.
+              </p>
+            </div>
+          </motion.div>
+          <motion.div
+            initial={{ x: '100%', opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.8, delay: 1.2 }}
+            className=' w-full flex flex-col gap-5 justify-center'
+          >
+            <div className="w-full max-w-4xl p-6 border border-accent rounded-lg transform transition duration-500 hover:scale-105 shadow-lg" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+              <p className="text-purple-300 text-lg">
+                Join us on our mission to revolutionize the legal landscape by providing free access to high-quality legal assistance.
+              </p>
+            </div>
+          </motion.div>
         </div>
-        <div className='w-2/3 flex flex-col gap-5 justify-center'>
-          <div className="animate-fade-in w-full max-w-4xl p-6 bg-primary border border-accent rounded-lg transform transition duration-500 hover:scale-105 shadow-lg">
-            <h2 className="text-purple-300 text-5xl md:text-6xl font-bold text-accent mb-4 leading-tight">
-              Innovating <br /> Legal Assistance with AI
-            </h2>
-            <p className="text-purple-300 text-lg">
-              LegalAI provides cutting-edge AI solutions to answer your legal questions quickly and accurately.
-            </p>
-          </div>
-          <div className="w-full max-w-4xl p-6 border border-accent rounded-lg transform transition duration-500 hover:scale-105 shadow-lg" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-            <p className="text-purple-300 text-lg">
-              Our goal is to make legal knowledge accessible, reliable, and efficient for everyone, by utilizing the latest advancements in AI technology.
-            </p>
-          </div>
-          <div className="w-full max-w-4xl p-6 border border-accent rounded-lg transform transition duration-500 hover:scale-105 shadow-lg" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-            <p className="text-purple-300 text-lg">
-              Join us on our mission to revolutionize the legal landscape by providing free access to high-quality legal assistance.
-            </p>
-          </div>
-        </div>
+
+        
+        
+        
       </div>
 
-      
       {/* New "Our Goal" box */}
-      <div className="w-full max-w-4xl p-6 border border-accent rounded-lg transform transition duration-500 hover:scale-105 shadow-lg" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+      <motion.div
+        ref={goalRef}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: goalInView ? 1 : 0 }}
+        transition={{ duration: 0.5 }}
+        className="w-full max-w-4xl p-6 border border-accent rounded-lg transform transition duration-500 hover:scale-105 shadow-lg"
+        style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+      >
         <h2 className="text-purple-300 text-4xl font-bold mb-4">Our Goal</h2>
         <p className="text-purple-300 text-lg">
           To democratize legal information by providing AI-driven solutions, ensuring that everyone has access to accurate and timely legal assistance, regardless of their background.
         </p>
-      </div>
+      </motion.div>
+
       {/* New "Hackathon Details" box */}
-      <div className="w-full max-w-4xl p-6 border border-accent rounded-lg transform transition duration-500 hover:scale-105 shadow-lg" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+      <motion.div
+        ref={hackathonRef}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: hackathonInView ? 1 : 0 }}
+        transition={{ duration: 0.5 }}
+        className="w-full max-w-4xl p-6 border border-accent rounded-lg transform transition duration-500 hover:scale-105 shadow-lg"
+        style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+      >
         <h2 className="text-purple-300 text-4xl font-bold mb-4">Hackathon Details</h2>
         <p className="text-purple-300 text-lg">
           We recently participated in a 48-hour hackathon, developing an AI-powered chatbot designed to provide quick legal advice. This project showcases our commitment to leveraging technology to make legal assistance accessible.
         </p>
-      </div>
-      <About/>
+      </motion.div>
+
+      <About />
     </section>
   );
 };
