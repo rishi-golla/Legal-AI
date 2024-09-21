902f5695b09f86131b446ccd307d41d502d99bbf
Vladislav Kondratyev <chikimoni61@gmail.com>
2024-09-21 15:12:00 -0500

    openAI + minor About update

--- legal-ai/src/components/About.js
+++ legal-ai/src/components/About.js
@@ -4,7 +4,7 @@ import Photo from './Photo';
 
 const teamMembers = [
   { name: 'Rishi Golla', role: 'Fullstack Developer + PM', image: 'rishi.jpeg' },
-  { name: 'Vladislav Kondratyev', role: 'Fullstack Developer + AI + PM', image: '../pic/' },
+  { name: 'Vladislav Kondratyev', role: 'Fullstack Developer + AI + PM', image: 'logo_vlad.png' },
   { name: 'Arnab Dev', role: 'Frontend Developer', image: '/arnab-t.png' },
   { name: 'Ankith Ganji', role: 'Backend Developer', image: 'ank.PNG' },
   { name: 'Aryav Rastogi', role: 'Backend Developer', image: '../pic/' },
@@ -16,8 +16,8 @@ const About = () => {
       <div className="mb-10 w-full max-w-4xl p-6 border border-accent rounded-lg transform transition duration-500 hover:scale-105 shadow-lg" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
         <h2 className="text-purple-300 text-4xl font-bold text-accent mb-4">About LegalAI</h2>
         <p className="text-purple-300 mb-4">
-          This project is a Hackathon initiative aiming to make legal assistance more accessible and efficient using AI.
-        </p>
+          This project, born out of a Hackathon initiative, aims to revolutionize legal assistance by making it more accessible and efficient through the power of AI.
+          </p>
       </div>
       <h3 className="text-purple-300 text-3xl font-semibold text-accent mb-8">Meet the Team</h3>
       <div className="flex items-center justify-center flex-wrap gap-4 w-[95%]">
