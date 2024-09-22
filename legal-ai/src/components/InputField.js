f02f87f8db8ee6c7636f227e433f238e12099d31
Arnab <official.arnabdev@gmail.com>
2024-09-22 14:23:20 -0500

    redesigned-chatbot

--- legal-ai/src/components/InputField.js
+++ legal-ai/src/components/InputField.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { FaArrowUp } from "react-icons/fa";
 
 const InputField = ({ input, onInputChange, onSend }) => {
   return (
-    <form onSubmit={onSend} className="mt-4 flex">
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => onInputChange(e.target.value)}
-        placeholder="Type your message..."
-        className="flex-1 p-2 border border-gray-300 rounded-md"
-      />
-      <button type="submit" className="ml-2 bg-indigo-600 text-white px-4 py-2 rounded-md">
-        Send
-      </button>
+    <form onSubmit={onSend} className="mt-4 flex w-full justify-center items-center">
+      <div className='md:w-[700px] w-full rounded-full border-[4px] justify-between flex'>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => onInputChange(e.target.value)}
+          placeholder="Type your message..."
+          className=" border-none outline-none bg-transparent pl-4 w-full text-white"
+        />
+        <button type="submit" className="ml-2 bg-purple-500 text-white p-2 rounded-full">
+          <FaArrowUp />
+        </button>
+      </div>
+      
     </form>
   );
 };
