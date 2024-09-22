8b1fb0a26953592db6a3d83d416e08483413994b
ch1kim0n1 <chikimoni61@gmail.com>
2024-09-22 15:37:26 -0500

    text fix

--- legal-ai/src/components/Messages.js
+++ legal-ai/src/components/Messages.js
@@ -4,18 +4,16 @@ const Messages = ({ messages }) => {
   return (
     <div className="bg-transparent p-4 rounded-md overflow-y-auto flex flex-col space-y-2 h-[400px] w-full">
       {messages.length === 0 ? (
-        <p className="text-gray-500 italic text-center">Hi! I'm Mike your lawyer, how can I help you today?</p>
+        <p className="text-gray-500 italic text-center">Hi! I'm Mike, your AI law assitant, how can I help you today?</p>
       ) : (
         messages.map((msg, index) => (
           <div
             key={index}
             className={`flex flex-col ${msg.sender === 'bot' ? 'items-start' : 'items-end'}`}
           >
-            {/* Sender Label */}
             <p className={`text-sm text-white opacity-80 mb-2 ${msg.sender == 'bot'? 'pl-3': 'pr-3'}`}>
               {msg.sender === 'bot' ? 'LegalAI' : 'You'}
             </p>
-            {/* Message Bubble */}
             <div
               className={`overflow-auto max-w-[50%] h-auto p-3 rounded-[20px] shadow-md ${msg.sender === 'bot'
                   ? 'bg-transparent border-2 border-white text-white' // Bot messages on the left
